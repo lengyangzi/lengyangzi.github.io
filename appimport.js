8b0fb0096d2a77f6a72b13894e55e9952e95072d
Xing <xing@XingdeMacBook-Pro.local>
2018-09-06 16:07:22 +0800

    Initial commit

--- appimport.js
+++ appimport.js
@@ -1,2 +1,13 @@
 console.log("恐惧让你沦为奴隶，希望使你重获自由。————《肖申克的救赎》")
-window.$crisp=[];window.CRISP_WEBSITE_ID="44cb21f7-fd9e-42cc-8c11-5a3e01ebe39a";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
+//window.$crisp=[];window.CRISP_WEBSITE_ID="44cb21f7-fd9e-42cc-8c11-5a3e01ebe39a";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
+
+// live chat
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5b67fe01df040c3e9e0c517c/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
